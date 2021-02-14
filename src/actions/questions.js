ea23935f8b14588d54d0b1545f9ba8aaca9221ad
Matthies Haentjens <haentjens.matthies@gmail.com>
2021-02-14 16:01:52 +0700

    feat: created initial action creators
    Created action creators for authedUser, questions and users. Action creators
    do not handle the api requests yet

---
+++ src/actions/questions.js
@@ -0,0 +1,26 @@
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
+export const ADD_QUESTION = 'ADD_QUESTION'
+
+export function receiveQuestions(questions) {
+    return {
+        type: RECEIVE_QUESTIONS,
+        questions,
+    } 
+}
+
+export function answerQuestion({ authedUser, id, answer }) {
+    return {
+        type: ANSWER_QUESTION,
+        authedUser,
+        id,
+        answer,
+    }
+}
+
+export function addQuestion(question) {
+    return {
+        type: ADD_QUESTION,
+        question,
+    }
+}
