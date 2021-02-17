5215000f63fd254c84914942fa8a5421ead5cb48
Matthies Haentjens <haentjens.matthies@gmail.com>
2021-02-17 11:11:06 +0700

    fix: adjusted the questions reducer
    Updated the questions reducer with comments to explain what is going on and
    corrected the answer question and add question case

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -4,21 +4,36 @@ export function questions(state = {}, action) {
     switch(action.type) {
         case RECEIVE_QUESTIONS:
             return {
+                // that has all the existing state data
                 ...state,
+                // that has all the questions data retrieved with the action
                 ...action.questions
             }
         case ANSWER_QUESTION:
             return {
+                // that has all the existing state data
                 ...state,
+                // that has all the exiting question data
+                ...state.questions,
+                // with a new question object that replaces the existing question
                 [action.id]: {
-                    ...state[action.id],
-                    [action.answer]: state[action.id].answer.votes.concat([action.authedUser])
+                    // that has all the existing question data
+                    ...state.questions[action.id],
+                    // with the user id added to votes of the chosen answer by the user
+                    [action.answer]: state.questions[action.id][action.answer].votes.concat([action.authedUser])
                 }
             }
         case ADD_QUESTION:
             return {
+                // that has all the existing state data
                 ...state,
-                ...action.question
+                // with a new questions object
+                questions: {
+                    // that has the existing questions data
+                    ...state.questions,
+                    // with a new question added
+                    [action.question.id]: action.question
+                }
             }
         default:
             return state
