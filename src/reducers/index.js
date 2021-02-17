2467b1efd34959c898735e695f8d205db060a868
Matthies Haentjens <haentjens.matthies@gmail.com>
2021-02-17 12:05:41 +0700

    feat: add the store to the app and removed loading bar
    Added the store to the root of the app. Additionally deleted the loading bar
    from react-redux-loading due to not being compatable with react 17.0.0

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
-import authedUser from './authedUser';
-import users from './users';
-import questions from './questions';
-import { loadingBarReducer } from 'react-redux-loading';
+import { authedUser } from './authedUser';
+import { users } from './users';
+import { questions } from './questions';
 
 export default combineReducers({
   authedUser,
   users,
   questions,
-  loadingBar: loadingBarReducer,
 });
